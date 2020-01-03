61bc1d26e41c400b38330a35a9754c078b0cc0e5
meRacle257 <zhuruofei2457@163.com>
2020-01-03 17:56:18 +0800

    init

---
+++ preprocess/convert2_ffm.py
@@ -0,0 +1,182 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+import sys, csv, math
+from collections import defaultdict
+import random
+import pandas as pd
+
+
+# In[2]:
+
+
+def sample(input_path, train_path, eval_path, test_path, ratio=0.8, debug=None, col_index=[]):
+    source = pd.read_csv(input_path, nrows=debug, header=None, names=col_index, sep='\t')
+    source = source.sample(frac=1.0)  # 全部打乱
+    cut_idx1 = int(round(0.1 * source.shape[0]))
+    cut_idx2 = int(round(0.3 * source.shape[0]))
+    
+    df_test, df_eval, df_train = source.iloc[:cut_idx1], source.iloc[cut_idx1:cut_idx2], source.iloc[cut_idx2:]
+    df_test.to_csv(test_path, index=None, sep=',')
+    df_eval.to_csv(eval_path, index=None, sep=',')
+    df_train.to_csv(train_path, index=None, sep=',')
+"""
+input_path = "./data/criteo.csv"
+train_file = "./cache/train.csv"
+eval_file = "./cache/eval.csv"
+test_file = "./cache/test.csv"
+fieldList = ['label','I1', 'I2', 'I3', 'I4', 'I5', 'I6', 'I7', 'I8', 'I9', 'I10', 'I11', 'I12', 'I13', 'C1', 'C2', 'C3',
+             'C4', 'C5', 'C6', 'C7', 'C8', 'C9', 'C10', 'C11', 'C12', 'C13', 'C14', 'C15', 'C16', 'C17', 'C18',
+             'C19', 'C20', 'C21', 'C22', 'C23', 'C24', 'C25', 'C26']
+sample(input_path, train_file, eval_file, test_file, debug=1000, col_index=fieldList)
+"""
+
+
+# In[3]:
+
+
+def scan(filename, feat_cnt):
+    for row in csv.DictReader(open(filename)):
+        for key, val in row.items():
+            if 'C' in key:
+                if val == '':
+                    feat_cnt[str(key) + '#' + 'absence'] += 1
+                else:
+                    feat_cnt[str(key) + '#' + str(val)] += 1
+"""
+# 统计category feat的词频
+feat_cnt = defaultdict(lambda: 0)
+scan(train_file, feat_cnt)
+scan(eval_file, feat_cnt)
+scan(test_file, feat_cnt)
+print(feat_cnt)
+"""
+
+
+# In[4]:
+
+
+
+"""
+# 离散特征判断为长尾的阈值
+T = 4 
+"""
+#考虑连续特征离散化和长尾特征之后，统计训练集和测试集的feat
+def get_feat(featSet, filename, tail=4):
+    for row in csv.DictReader(open(filename)):
+        for key, val in row.items():
+            if 'I' in key and key != "Id":
+                if val == '':
+                    featSet.add(str(key) + '#' + 'absence')
+                else:
+                    val = int(float(val))
+                    if val > 2:
+                        val = int(math.log(float(val)) ** 2) # 离散化
+                    else:
+                        val = 'SP' + str(val)
+                    featSet.add(str(key) + '#' + str(val))
+                continue
+            if 'C' in key:
+                if val == '':
+                    feat = str(key) + '#' + 'absence'
+                else:
+                    feat = str(key) + '#' + str(val)
+                if feat_cnt[feat] > tail:
+                    featSet.add(feat)
+                else:
+                    featSet.add(str(key) + '#' + str(feat_cnt[feat])) # 长尾数据不计value,只计频次
+                continue
+"""
+T = 4
+featSet = set()
+get_feat(featSet, train_file, T)
+get_feat(featSet, test_file, T)
+get_feat(featSet, eval_file, T)
+print('train and test data total feat num:', len(featSet))
+"""
+
+
+# In[5]:
+
+
+def convert_to_ffm(src_path, dst_path, fieldIndex, featIndex):
+    out = open(dst_path, 'w')
+    for row in csv.DictReader(open(src_path)):
+        feats = []
+        feats.append(row['Label'])
+        for key, val in row.items():
+            if key == 'Label':
+                continue
+            if 'I' in key and key != "Id":
+                if val == '':
+                    feat = str(key) + '#' + 'absence'
+                else:
+                    val = int(float(val))
+                    if val > 2:
+                        val = int(math.log(float(val)) ** 2)
+                    else:
+                        val = 'SP' + str(val)
+                    feat = str(key) + '#' + str(val)
+                feats.append(str(fieldIndex[key]) + ':' + str(featIndex[feat]) + ':1')
+                continue
+            if 'C' in key:
+                if val == '':
+                    feat = str(key) + '#' + 'absence'
+                else:
+                    feat = str(key) + '#' + str(val)
+                if feat_cnt[feat] > T:
+                    feat = feat
+                else:
+                    feat = str(key) + '#' + str(feat_cnt[feat])
+                feats.append(str(fieldIndex[key]) + ':' + str(featIndex[feat]) + ':1')
+                continue
+        out.write(' '.join(feats) + '\n')
+    out.close()
+
+
+# In[26]:
+
+
+if __name__ == '__main__':
+    input_path = "./data/criteo.csv"
+    train_file = "./cache/train.csv"
+    eval_file = "./cache/eval.csv"
+    test_file = "./cache/test.csv"
+    fieldList = ['Label', 'I1', 'I2', 'I3', 'I4', 'I5', 'I6', 'I7', 'I8', 'I9', 'I10', 'I11', 'I12', 'I13', 'C1', 'C2', 'C3',
+                 'C4', 'C5', 'C6', 'C7', 'C8', 'C9', 'C10', 'C11', 'C12', 'C13', 'C14', 'C15', 'C16', 'C17', 'C18',
+                 'C19', 'C20', 'C21', 'C22', 'C23', 'C24', 'C25', 'C26']
+    sample(input_path, train_file, eval_file, test_file, debug=100000, col_index=fieldList)
+    
+    # 统计category feat词频
+    feat_cnt = defaultdict(lambda: 0)
+    scan(train_file, feat_cnt)
+    scan(eval_file, feat_cnt)
+    scan(test_file, feat_cnt)
+    T = 4 # category feat长尾阈值
+    featSet = set()
+    # continuous feat离散化  category feat去长尾
+    get_feat(featSet, train_file, T)
+    get_feat(featSet, test_file, T)
+    get_feat(featSet, eval_file, T)
+    print('train and test data total feat num:', len(featSet))
+
+    # 特征值编号
+    featIndex = dict()
+    for index, feat in enumerate(featSet, start=1):
+        featIndex[feat] = index
+        # print(index, feat)
+    print('feat dict num:', len(featIndex))
+
+    # 特征名（域）编号
+    fieldIndex = dict()
+    for index, field in enumerate(fieldList, start=0):
+        fieldIndex[field] = index
+    print('field dict num:', len(fieldIndex))
+    
+    convert_to_ffm(train_file, "./data/train.ffm", fieldIndex, featIndex)
+    convert_to_ffm(eval_file, "./data/eval.ffm", fieldIndex, featIndex)
+    convert_to_ffm(test_file, "./data/test.ffm", fieldIndex, featIndex)
+
